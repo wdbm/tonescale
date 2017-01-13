73cb41cbe5ac2360858e78e09461221bb162e7ce
Will Breaden Madden <zicodian@gmail.com>
2017-01-13 13:44:39 +0000

    add include_package_data to setup.py

--- setup.py
+++ setup.py
@@ -8,26 +8,27 @@ import setuptools
 def main():
 
     setuptools.setup(
-        name             = "tonescale",
-        version          = "2017.01.12.2315",
-        description      = "sound utilities and sounds",
-        long_description = pypandoc.convert("README.md", "rst"),
-        url              = "https://github.com/wdbm/tonescale",
-        author           = "Will Breaden Madden",
-        author_email     = "wbm@protonmail.ch",
-        license          = "GPLv3",
-        py_modules       = [
-                           "tonescale"
-                           ],
-        install_requires = [
-                           "numpy",
-                           "scipy",
-                           "shijian"
-                           ],
-        scripts          = [
-                           "sound_search.py"
-                           ],
-        entry_points     = """
+        name                 = "tonescale",
+        version              = "2017.01.13.1331",
+        description          = "sound utilities and sounds",
+        long_description     = pypandoc.convert("README.md", "rst"),
+        url                  = "https://github.com/wdbm/tonescale",
+        author               = "Will Breaden Madden",
+        author_email         = "wbm@protonmail.ch",
+        license              = "GPLv3",
+        include_package_data = True,
+        py_modules           = [
+                               "tonescale"
+                               ],
+        install_requires     = [
+                               "numpy",
+                               "scipy",
+                               "shijian"
+                               ],
+        scripts              = [
+                               "sound_search.py"
+                               ],
+        entry_points         = """
             [console_scripts]
             tonescale = tonescale:tonescale
         """
