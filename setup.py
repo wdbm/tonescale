45bf5baf47f1ed1107eab51d254d20879020162e
Will Breaden Madden <wbm@protonmail.ch>
2018-02-06 19:23:42 +0000

    add play sound and play alarm functions

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def main():
 
     setuptools.setup(
         name                 = "tonescale",
-        version              = "2017.07.04.1100",
+        version              = "2018.02.06.1919",
         description          = "sound utilities and sounds",
         long_description     = long_description(),
         url                  = "https://github.com/wdbm/tonescale",
